92803d8e568ecb50f2a11480412df68da98f4eaf
Pete Xu <pengx@alum.lehigh.edu>
2023-11-16 21:45:48 -0500

    add form

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,6 +10,11 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	// custom not found handler
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notfound(w)
+	})
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static/", fileServer))
